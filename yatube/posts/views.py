2a8064dc81f703c19a1a7edceb8cd91a2ba4523d
nmashkov <nmashkov@gmail.com>
2022-11-15 21:41:49 +0300

    Initial Yatube project stage

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,24 @@
-# posts/views.py
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
-# Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Публикация номер {slug}') 
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = f'Записи сообщества {str(group)}'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/group_list.html', context)
